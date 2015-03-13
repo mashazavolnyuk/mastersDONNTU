feea835880dc2007a2b3d90262bb0af0194e9258
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2015-03-13 17:47:36 +0200

    fixed leftPanel

---
+++ scripts/language.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Merry on 13.03.2015.
+ */
+function load_default(){
+    $("#text_content").load("language/english.html #Resume_english > *");
+}
+
--- scripts/simpleScript.js
+++ scripts/simpleScript.js
@@ -1,18 +1,8 @@
 /**
  * Created by Merry on 11.03.2015.
  */
-function HideOrOpenPanel(id_panel){
-    var panel=document.getElementById(id_panel);
-    var mainId = document.getElementById(element_id);
 
-    if (panel.style.display != "block") {
-        panel.style.display = "block"; //Показываем элемент
-    }
-    else {
-        panel.style.display = "none";
-    } //Скрываем элемент
+$( "sandwich" ).click(function() {
+    $( "#left_panel").hide( "slow" );
+});
 
-
-
-
-}
