e21baa3aaa9846ed1c98816c6a7056da49a82a79
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2015-05-24 01:13:29 +0300

    added Marvel

---
+++ scripts/slider.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Merry on 23.05.2015.
+ */
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        infiniteLoop: false,
+        hideControlOnEnd: true
+    });
+});
