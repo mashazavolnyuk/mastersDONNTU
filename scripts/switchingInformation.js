7fa1a6977b4eed75fc5e573db2e43c8456821e63
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2015-06-10 21:19:15 +0300

    added topInformation,library

--- scripts/switchingInformation.js
+++ scripts/switchingInformation.js
@@ -4,7 +4,6 @@
 var currentLanguage;
 
 
-
 function switchInformation(item) {
     //item - номер из списка
     //0-resume
@@ -16,12 +15,15 @@ function switchInformation(item) {
             switch (currentLanguage) {
                 case 0:
                     $("#contentText").load("language/ru/ru_resume.html #Resume_russian > *");
+                    setTopText(0);
                     break;
                 case 2:
                     $("#contentText").load("language/en/en_resume.html #Resume_english > *");
+                    setTopText(2);
                     break;
                 case 1:
                     $("#contentText").load("language/uk/uk_resume.html #Resume_uk> *");
+                    setTopText(1);
                     break;
             }//switch
             break;//case 0
@@ -29,9 +31,11 @@ function switchInformation(item) {
             switch (currentLanguage) {
                 case 0:
                     $("#contentText").load("language/ru/ru_bio.html #AboutMyself > *");
+                    setTopText(0);
                     break;
                 case 1:
                     $("#contentText").load("language/uk/uk_bio.html #AboutMyself > *");
+                    setTopText(1);
                     break;
             }//switch
             break;//case 1
@@ -40,32 +44,51 @@ function switchInformation(item) {
             switch (currentLanguage) {
                 case 0:
                     $("#contentText").load("language/ru/ru_abstract.html #abstract > *");
+                    setTopText(0);
                     break;
                 case 1:
                     $("#contentText").load("language/uk/uk_abstract.html #abstract > *");
+                    setTopText(1);
                     break;
                 case 2:
                     $("#contentText").load("language/en/en_adstract.html #en-abstract > *");
+                    setTopText(2);
                     break;
             }//switch
             break;//case 2
         case 3:
             $("#contentText").load("language/ru/ru_library.html #library > *");
+            setTopText(0);
             break;
         case 4:
             $("#contentText").load("language/ru/ru_reference.html #refer > *");
+            setTopText(0);
             break;
         case 5:
             $("#contentText").load("language/ru/ru_report.html .GlobalReport > *");
-
+            setTopText(0);
             break;//case 5
     }
 }//switchInformation
 
 function CleanContent() {
-
     $('#contentText').empty();
 }
+
+function setTopText(numerLanguge) {
+    switch (numerLanguge) {
+        case 0:
+            $("#topTextPanel").load("language/ru/ru_topInformation.html #top_russian> *");
+            break;
+        case 1:
+            $("#topTextPanel").load("language/uk/uk_topInformation.html #top_ukrain> *");
+            break;
+        case 2:
+            $("#topTextPanel").load("language/en/en_topInformation.html #top_english> *");
+            break;
+    }
+}
+
 function setCurrentLanguage(numerLanguge) {
 
     switch (numerLanguge) {
@@ -74,12 +97,17 @@ function setCurrentLanguage(numerLanguge) {
             $("#en_left_panel").css('display', 'none');
             $('#ru_left_panel').css('display', 'block');
             $('#uk_left_panel').css('display', 'none');
+            $("#contentText").load("language/ru/ru_resume.html #Resume_russian > *");
+            setTopText(0);
             break;
         case 1:
             currentLanguage = 1;
             $("#en_left_panel").css('display', 'none');
             $('#ru_left_panel').css('display', 'none');
             $('#uk_left_panel').css('display', 'block');
+            $("#contentText").load("language/uk/uk_resume.html #Resume_uk> *");
+            setTopText(1);
+            ;
             break;
         case 2:
         {
@@ -87,9 +115,10 @@ function setCurrentLanguage(numerLanguge) {
             $("#en_left_panel").show();
             $('#ru_left_panel').css('display', 'none');
             $('#uk_left_panel').css('display', 'none');
+            $("#contentText").load("language/en/en_resume.html #Resume_english > *");
+            setTopText(2);
             break;
         }
-
     }
 
 }
